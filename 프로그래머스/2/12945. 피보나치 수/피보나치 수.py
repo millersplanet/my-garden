419815319552137c7f6abd00829610b86321fd2a
June <frommillersplanet@gmail.com>
2024-09-03 18:48:58 +0900

    Update 피보나치 수.py

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -3,6 +3,6 @@ def solution(n):
     fib[0], fib[1] = 0, 1
 
     for i in range(2, n + 1):
-        fib[i] = (fib[i - 1] + fib[i - 2]) % 1234567
+        fib[i] = fib[i - 1] + fib[i - 2]
 
-    return fib[n]
+    return fib[n] % 1234567
